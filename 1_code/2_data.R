59070098dd0dfd2e0c677a7c5356e31a45cf5238
Christopher Boyer <christopherbboyer@gmail.com>
2022-02-16 23:03:47 -0500

    create long version of dataset

--- 1_code/2_data.R
+++ 1_code/2_data.R
@@ -7,8 +7,8 @@ file_list <- c(
   "Exam3/Data/mesae3dres06222012.sas7bdat",
   "Exam4/Data/mesae4dres06222012.sas7bdat",
   "Exam5/Data/mesae5_drepos_20210920.sas7bdat",
-  "Events/CVD/Data/mesaevthr2015_drepos_20200330.sas7bdat",
-  "Events/NonCVD/Data/mesaevnoncvddres06192012.sas7bdat"
+  "Events/CVD/Data/mesaevthr2015_drepos_20200330.sas7bdat"
+  # "Events/NonCVD/Data/mesaevnoncvddres06192012.sas7bdat"
 )
 
 dats <- lapply(file_list, function (x) {
@@ -23,8 +23,8 @@ names(dats) <- c(
   "exam3",
   "exam4",
   "exam5",
-  "cvd",
-  "noncvd"
+  "cvd"
+  # "noncvd"
 )
 
 
@@ -135,3 +135,30 @@ dats <- lapply(seq(1, length(dats)), function(x) {
 mesa <- Reduce(function(df1, df2) left_join(df1, df2, by = id_vars), dats)
 
 rm(dats)
+
+
+# create long version -----------------------------------------------------
+
+mesa <- 
+  mesa %>%
+  rename(
+    time2 = e12dyc,
+    time3 = e13dyc,
+    time4 = e14dyc,
+    time5 = e15dyc
+  ) 
+
+# pivot to longer format
+mesa_long <- 
+  mesa %>%
+  pivot_longer(
+    cols = matches(".*[2-5]c?$"),
+    names_to = c("variable", "exam"),
+    names_pattern = "(.*)([2-5])"
+  ) %>%
+  pivot_wider(
+    names_from = "variable", 
+    values_from = "value"
+  )
+
+

56c4653b798162ed3324fe8b3b3746022b3e4adf
Christopher Boyer <christopherbboyer@gmail.com>
2022-02-24 15:35:16 -0500

    move pivot longer

--- 1_code/2_data.R
+++ 1_code/2_data.R
@@ -164,28 +164,3 @@ mesa <- Reduce(function(df1, df2) left_join(df1, df2, by = id_vars), dats)
 rm(dats)
 
 
-# create long version -----------------------------------------------------
-
-mesa <- 
-  mesa %>%
-  rename(
-    time2 = e12dyc,
-    time3 = e13dyc,
-    time4 = e14dyc,
-    time5 = e15dyc
-  ) 
-
-# pivot to longer format
-mesa_long <- 
-  mesa %>%
-  pivot_longer(
-    cols = matches(".*[2-5]c?$"),
-    names_to = c("variable", "exam"),
-    names_pattern = "(.*)([2-5])"
-  ) %>%
-  pivot_wider(
-    names_from = "variable", 
-    values_from = "value"
-  )
-
-

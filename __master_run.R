daf0449835b8978762a6cc3adad3ad73b3d336b4
Christopher Boyer <christopherbboyer@gmail.com>
2022-03-07 23:01:01 -0500

    Update __master_run.R

--- __master_run.R
+++ __master_run.R
@@ -7,5 +7,15 @@ source("1_code/2_data.R")
 
 source("1_code/3_cleaning.R")
 
-source("1_code/4_analysis.R")
+source("1_code/4_exclusion.R")
+
+source("1_code/5_imputations.R")
+
+source("1_code/6_target_trial.R")
+
+source("1_code/7_main_analysis.R")
+
+source("1_code/X1_sensitivity.R")
+
+
 

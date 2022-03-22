710d517918036aa448ee9218eb0c61e72730bdd6
Christopher Boyer <christopherbboyer@gmail.com>
2022-03-21 23:50:00 -0400

    Update 9_counterfactual_risk.R

--- 1_code/9_counterfactual_risk.R
+++ 1_code/9_counterfactual_risk.R
@@ -32,7 +32,7 @@ risk_df <-
 # record risk factor means prior to censoring
 risk_factor_means <- 
   risk_df %>%
-  summarise(across(all_of(baseline_vars_long), mean))
+  summarise(across(all_of(c(baseline_vars_long, "bl_chol")), mean))
   
 # center risk factors prior to fitting
 risk_df <- 
@@ -98,17 +98,17 @@ tbl_merge(
 # first let's look at thresholds based on MESA risk distribution
 preds <- map_dfc(c(fit_fct, fit_cft), ~compute_risk_score(.x, risk_df, 120))
 
-risk_df <-
+risk_df_long <-
   risk_df %>%
   bind_cols(preds) %>%
   pivot_longer(c(Factual, Counterfactual))
 
 p3 <- 
-  ggplot(risk_df, aes(x = value, color = name)) +
+  ggplot(risk_df_long, aes(x = value, color = name)) +
   geom_line(aes(y = 1 - ..y..), stat='ecdf') +
   theme_classic() +
   # annotate("text", x = 0.085, y = 0.54, label = "0.53", ) +
-  scale_x_continuous(breaks = seq(0, 0.6, by = 0.05), minor_breaks = T) +
+  scale_x_continuous(limits = c(0, 0.6), breaks = seq(0, 0.6, by = 0.05), minor_breaks = T) +
   scale_color_brewer(name = "", palette = "Set1") +
   labs(
     x = "\nRisk treshold for treatment",
@@ -122,19 +122,37 @@ ggsave("3_figures/treatment_curves.pdf", p3, device = "pdf", width = 6, height =
 
 # now let's look at a US representative population from 2017-2018 NHANES
 
+nhanes_data <-
+  nhanes_data %>%
+  mutate(
+    # convert continuous risk factors to larger units to be more readily interpretable
+    bl_age = as.numeric(bl_age) / 5,    # age in 5 year increments
+    bl_sbp = bl_sbp / 10,   # SBP in 10 mmHg increments
+    bl_hdl = bl_hdl / 10,   # HDL in 10 mg/dL increments
+    bl_chol = bl_chol / 10,  # total cholesterol in 10 mg/dL increments
+    
+    bl_age = bl_age - risk_factor_means[['bl_age']],
+    bl_sbp = bl_sbp - risk_factor_means[['bl_sbp']],
+    bl_hdl = bl_hdl - risk_factor_means[['bl_hdl']],
+    bl_chol = bl_chol - risk_factor_means[['bl_chol']]
+  ) 
+
 preds <- map_dfc(c(fit_fct, fit_cft), ~compute_risk_score(.x, nhanes_data, 120))
 
-nhanes_data <-
+nhanes_data_long <- 
   nhanes_data %>%
   bind_cols(preds) %>%
   pivot_longer(c(Factual, Counterfactual))
 
+
+svydesign(id = ~SDMVPSU, strata = ~SDMVSTRA, weights = ~WTINT2YR, data = nhanes_data_long, nest = T)
+
 p4 <-
-  ggplot(nhanes_data, aes(x = value, color = name)) +
+  ggplot(nhanes_data_long, aes(x = value, color = name)) +
   geom_line(aes(y = 1 - ..y..), stat='ecdf') +
   theme_classic() +
   # annotate("text", x = 0.085, y = 0.54, label = "0.53", ) +
-  scale_x_continuous(breaks = seq(0, 0.6, by = 0.05), minor_breaks = T) +
+  scale_x_continuous(limits = c(0, 0.6), breaks = seq(0, 0.6, by = 0.05), minor_breaks = T) +
   scale_color_brewer(name = "", palette = "Set1") +
   labs(
     x = "\nRisk treshold for treatment",
@@ -144,4 +162,40 @@ p4 <-
   geom_vline(xintercept = c(0.075, 0.10, 0.15), linetype = "dotted")
 
 ggsave("3_figures/treatment_curves_nhanes.pdf", p4, device = "pdf", width = 6, height = 4)
+
+nhanes_design <-
+  svydesign(
+    id = ~ SDMVPSU,
+    strata = ~ SDMVSTRA,
+    weights = ~ WTINT2YR,
+    data = nhanes_data_long,
+    nest = TRUE
+  )
+
+cdf_cft <- svycdf(~value, subset(nhanes_design, name == "Counterfactual"))
+cdf_fct <- svycdf(~value, subset(nhanes_design, name == "Factual"))
+
+expand_grid(
+  name = c("Factual", "Counterfactual"),
+  x = seq(0, 1, by = 0.0005),
+) %>%
+  mutate(
+    y = 1 - if_else(name == "Factual", cdf_fct$value(x), cdf_cft$value(x))
+  ) %>%
+  ggplot(., aes(x = x, y = y, color = name)) +
+  geom_step() +
+  theme_classic() +
+  # annotate("text", x = 0.085, y = 0.54, label = "0.53", ) +
+  scale_x_continuous(limits = c(0, 0.6), breaks = seq(0, 0.6, by = 0.05), minor_breaks = T) +
+  scale_color_brewer(name = "", palette = "Set1") +
+  labs(
+    x = "\nRisk treshold for treatment",
+    y = "Proportion treated\n"
+  ) +
+  theme(legend.position = c(0.8, 0.8)) +
+  geom_vline(xintercept = c(0.075, 0.10, 0.15), linetype = "dotted")
+
+
+
+
 
